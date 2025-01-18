acac954569bcf0339b005e66ae95c544072a690b
YoungJun <coke05288@gmail.com>
2025-01-19 00:33:33 +0900

    25.01.19 SUN | Set Default Project

---
+++ src/main/java/com/nemostudio/aras_spring/ArasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nemostudio.aras_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArasSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArasSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nemostudio/aras_spring/ArasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemostudio.aras_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArasSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
